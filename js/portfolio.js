4274e097334655e0cc7cda659ff7cf11f03155cf
Ashish Mundra <Ashish.Mundra@ap.jll.com>
2021-10-02 01:18:03 +1000

    Added next prev arrow

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,8 +1,17 @@
 $(function(){
     $('.portfolioslider').slick({
       arrows: false,
-      dots: true,
-      infinite: true,
-      slidesToShow: 1        
+      adaptiveHeight: true
     });
+
+    // Custom carousel nav
+    $('.carousel-prev').click(function(){ 
+      $(this).parent().find('.slick-slider').slick('slickPrev');
+    } );
+    
+    $('.carousel-next').click(function(e){
+      e.preventDefault(); 
+      $(this).parent().find('.slick-slider').slick('slickNext');
+    });
+
   });
